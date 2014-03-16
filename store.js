77065a72a57c50ef2d9e0014f4d607da012243b8
Danish Mujeeb <danish.mujeeb@gmail.com>
2014-03-16 10:16:45 -0400

    Added all the questions in the game

--- store.js
+++ store.js
@@ -12,9 +12,38 @@ var GAME = {
                 'Shift into gear 1',
                 'Step on accelerator'
             ]
+        }, {
+            text: 'How to fix a hole in the Drywall',
+            steps: [
+                'Clean hole edges',
+                'Cut plasterboard patch',
+                'Clean & Dry Area',
+                'Apply Patch',
+                'Apply Putty',
+                'Apply sanding paper'
+            ]
+        }, {
+            text: 'Put the following Star Trek TV series in correct order',
+            steps: [
+                'Star Trek - Original Series',
+                'Star Trek - The Next Generation',
+                'Star Trek - Deep Space Nine',
+                'Star Trek - Voyager',
+                'Star Trek - Enterprise'
+            ]
+        }, {
+            text: 'What is the sequence of notes on a Guitar?'
+            steps: [
+                'E',
+                'A',
+                'D',
+                'G',
+                'B',
+                'E'
+            ]
         }]
     }, {
-        name: 'Round 2 - Girl Stuff',
+        name: 'Round 2 - Girly Stuff',
         questions: [{
             text: 'How to wash hair during a shower?',
             steps: [
@@ -23,6 +52,42 @@ var GAME = {
                 'Rinse Hair',
                 'Apply Conditioner',
                 'Rinse Hair'
+            ]
+        }, {
+            text: 'How to apply makeup for a wedding?'
+            steps: [
+                'Apply Moisturizer',
+                'Apply Foundation',
+                'Apply Eye Liner',
+                'Apply Blushon',
+                'Apply Lipstick'
+            ]
+        }, {
+            text: 'How to wear a Sari?'
+            steps: [
+                'Wear Petticoat',
+                'Wear Blouse',
+                'Tuck Sari with 1 turn',
+                'Make pleats',
+                'Drape over shoulder'
+            ]
+        }, {
+            text: 'Put these Indian movies in the right order of release date'
+            steps: [
+                'Kabhi Kabhi',
+                'American Desi',
+                'Three Idiots',
+                'Slumdog Millionaire',
+                'Jab Tak Hai Jaan'
+            ]
+        }, {
+            text: 'How to make a Rus-Gullah'
+            steps: [
+                'Make Dough',
+                'Make Syrup',
+                'Make Balls',
+                'Fry Balls',
+                'Soak it in syrup'
             ]
         }]
     }]
